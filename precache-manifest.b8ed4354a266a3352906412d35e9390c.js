ee01f201452e7281f9801f403b4cd34702bdd9ad
radlee <prof.lolito@gmail.com>
2020-09-11 03:39:15 +0200

    Updates

---
+++ precache-manifest.b8ed4354a266a3352906412d35e9390c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d82ff533d9479dc2f740985a667350",
+    "url": "/amphotography/index.html"
+  },
+  {
+    "revision": "645db42b66bb9d4590b8",
+    "url": "/amphotography/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "920f491aded274ef0b50",
+    "url": "/amphotography/static/css/main.88f5c016.chunk.css"
+  },
+  {
+    "revision": "645db42b66bb9d4590b8",
+    "url": "/amphotography/static/js/2.aed67996.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/amphotography/static/js/2.aed67996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920f491aded274ef0b50",
+    "url": "/amphotography/static/js/main.4d1aeaa3.chunk.js"
+  },
+  {
+    "revision": "538b89b97e08310f317b",
+    "url": "/amphotography/static/js/runtime-main.4fdfdf6a.js"
+  },
+  {
+    "revision": "c92865021200c9a048d1563e12b74c08",
+    "url": "/amphotography/static/media/amp.c9286502.jpg"
+  },
+  {
+    "revision": "0d694d0ec2c2f7e2d24513ffebd89e08",
+    "url": "/amphotography/static/media/bg31.0d694d0e.jpg"
+  },
+  {
+    "revision": "caf65043ebf9b0274c24de8cc5daf673",
+    "url": "/amphotography/static/media/last.caf65043.png"
+  },
+  {
+    "revision": "7853cd2c5515aecec890197805364e28",
+    "url": "/amphotography/static/media/mp.7853cd2c.jpg"
+  },
+  {
+    "revision": "5fc2f941d74fb33a9c7fb11caf9fb4d6",
+    "url": "/amphotography/static/media/one.5fc2f941.jpg"
+  },
+  {
+    "revision": "1b98cce47be6b8fc192ba294d242e986",
+    "url": "/amphotography/static/media/three.1b98cce4.JPG"
+  },
+  {
+    "revision": "8634aa32e2c8db751e7c20c3f4e1af1d",
+    "url": "/amphotography/static/media/two.8634aa32.JPG"
+  }
+]);
